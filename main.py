3d5abe5d50d15cf9e582d7cd301ff70887b72aaf
GustavoeDev <gustavoe.dev@gmail.com>
2025-05-26 15:36:50 -0300

    feat: add websocket server

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi_socketio import SocketManager
 
 app = FastAPI()
+
+socket_manager = SocketManager(app=app, cors_allowed_origins="*")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
